60e6c73fefb768f44b27c519b3d75eb88134ea3a
Olga Tereschenko <nice.olganikolaevna@mail.ru>
2021-11-15 17:42:41 +0300

    ДЗ№7

--- react-lesson-1/src/App.js
+++ react-lesson-1/src/App.js
@@ -1,13 +1,8 @@
 import { ChatHomework } from "./components/index";
-import { Provider } from "react-redux";
-import {store} from "./store/index";
+
  
 const App=()=> {
-  return (
-  <Provider store={store}> 
-    <ChatHomework/>
-  </Provider>
-  )
+  return <ChatHomework/>
 }
 
 export default App;
